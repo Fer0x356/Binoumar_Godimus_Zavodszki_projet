0b7d4a1bd0874867bcb56125b3c82de0c5615f63
Amine <93395428+AmineBin@users.noreply.github.com>
2025-10-13 19:18:21 +0200

    Changement du chemin pour lancer le jeu

--- HUD/Menu/menu.gd
+++ HUD/Menu/menu.gd
@@ -1,5 +1,5 @@
 extends Control
-@onready var game = preload("res://player.tscn")
+@onready var game = preload("res://main.tscn")
 
 func _on_btn_start_button_down() -> void:
 	get_tree().change_scene_to_packed(game)	
